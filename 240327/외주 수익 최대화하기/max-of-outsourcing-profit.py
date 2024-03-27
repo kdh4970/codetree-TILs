74cf6517420026f6d161578b65a4483d35c3a898
kdh4970 <100071841+kdh4970@users.noreply.github.com>
2024-03-27 19:42:15 +0900

    Title: 외주 수익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240327/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,25 @@
+# n 일간 외주 개발
+# 1일부터 day에 t[i]를 더하고, money에 p[i]를 더해가며
+# day가 n+1 이면 종료
+
+N = int(input())
+t = []
+p = []
+for _ in range(N):
+    temp_t,temp_p = list(map(int,input().split()))
+    t.append(temp_t)
+    p.append(temp_p)
+lst = []
+
+def func(day,money):
+    if day == N+1:
+        lst.append(money)
+        return
+    else:
+        func(day+t[day-1],money+p[day-1])
+        func(day+1,money)
+
+
+
+func(1,0)
+print(max(lst))
