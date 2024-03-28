0741030c4aa618896c9c5cfd2e642c8eaa1ea343
kdh4970 <100071841+kdh4970@users.noreply.github.com>
2024-03-28 20:48:44 +0900

    Title: 불안한 무빙워크, Time: 5060ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240328/불안한 무빙워크/unstable-moving-walk.py
+++ 240328/불안한 무빙워크/unstable-moving-walk.py
@@ -1,28 +1,45 @@
 # 무빙워크 판의 개수 N, 안정성이 0인 판의 개수가 K일 떄 실험 종료
 # 종료 조건 : 안정성이 0인 판이 K개
 # 
-import itertools
+from collections import deque
 
 N, K = list(map(int,input().split()))
-moving_walk = list(map(int,input().split()))
+moving_walk = deque(list(map(int,input().split())))
 
 
-# def simul():
-#     cnt = 0
-#     person = []
-#     while True:
-#         #1
-#         cnt += 1
-#         # person += 1
-#         #2
-#         if person:
-#             for _ in range(len(person)):
-#                 if person[_+1] or moving_walk[person[_]-cnt]
+def simul():
+    cnt = 0
+    person = deque()
+    while True:
+        #1
+        cnt += 1
+        moving_walk.rotate(1)
+        if person:
+            for _ in range(len(person)):
+                person[_] += 1
+            if N-1 in person:
+                person.popleft()
+        #2
+        if person:
+            for _ in range(len(person)):
+                if person[_]+1 in person or moving_walk[person[_]] == 0:
+                    continue
+                person[_] += 1
+                moving_walk[person[_]] -= 1
+            
+            if N-1 in person:
+                person.popleft()
+        #3
+        if 0 not in person and moving_walk[0] > 0:
+            person.append(0)
+            moving_walk[0] -=1
+            if N-1 in person:
+                    person.popleft()
+        #4
+        # print(moving_walk)
+        # print(person)
+        if moving_walk.count(0) >= K:
+            return cnt
+        
 
-#         #3
-
-#         #4
-#         if moving_walk.count(0) == K:
-#             return cnt
-
-# print(simul())
+print(simul())
