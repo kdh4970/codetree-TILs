24363a0aac4c2981537fbd87efd4e794c569b21f
kdh4970 <100071841+kdh4970@users.noreply.github.com>
2024-03-28 19:19:31 +0900

    Title: 조삼모사, Time: 635ms, Memory: 54MB, Status: Passed - CodeTree

--- 240328/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240328/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -1,3 +1,4 @@
+# 1074 Z
 # n개의 일이 주어질 때, 아침 저녁으로 n/2개 씩 처리
 # 나누어서 하는 일이 2,3,4,5일때
 # 더해야 할 강도 수 1*2,3*2,6*2, 10*2
@@ -8,16 +9,16 @@
 # n 개의 일을 n/2개로 분할 시 나오는 경우의 수 k, k[0] k[-1]은 아침 저녁 일 관계, 즉 코스트도 k개에 대해서 구하고, 양끝에서부터 합계산
 # 강도를 구하는 함수
 
-from functools import lru_cache
+# from functools import lru_cache
 
 N = int(input())
 p = [list(map(int,input().split())) for _ in range(N)]
 
-@lru_cache
+# @lru_cache
 def getCost(job1,job2):
     return p[job1][job2] + p[job2][job1]
 
-@lru_cache
+# @lru_cache
 def getTotalCost(job_list):
     total = 0
     for job1 in job_list:
@@ -44,7 +45,7 @@ def AllocJob(job_num):
 
 AllocJob(0)
 
-min_cost_diff = 100
+min_cost_diff = 2**32-1
 for _ in range(len(cost_list)//2):
     day_cost = cost_list[_]
     night_cost = cost_list[len(cost_list)-1-_]
