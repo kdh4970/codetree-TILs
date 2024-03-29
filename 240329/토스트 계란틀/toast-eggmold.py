654de9836ec6357a8a23167051f81b6ec31dff85
kdh4970 <100071841+kdh4970@users.noreply.github.com>
2024-03-29 13:56:01 +0900

    Title: 토스트 계란틀, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240329/토스트 계란틀/toast-eggmold.py
+++ 240329/토스트 계란틀/toast-eggmold.py
@@ -4,12 +4,13 @@ N,L,R = tuple(map(int,input().split()))
 board = [list(map(int,input().split())) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
 stop = False
+moved = False
 
 dr = [1,-1,0,0]
 dc = [0,0,1,-1]
 
 def bfs(row,col):
-    global stop
+    global stop, moved
     q = deque()
     q.append((row,col))
     union = []
@@ -29,6 +30,7 @@ def bfs(row,col):
         stop = True
     else:
         stop = False
+        moved = True
         total = 0
         for r,c in union:
             total += board[r][c]
@@ -38,11 +40,12 @@ def bfs(row,col):
 
 cnt = 0
 while not stop:
-    cnt += 1
+    moved = False
     for r in range(N):
         for c in range(N):
             if visited[r][c] == True:
                 continue
             bfs(r,c)
+    if moved : cnt+=1
 
 print(cnt)
